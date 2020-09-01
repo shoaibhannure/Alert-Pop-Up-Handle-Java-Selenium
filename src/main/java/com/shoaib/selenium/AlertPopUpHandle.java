83fe7eded5b56f27268f86aa3c4fa223944b58c3
shoaib_hannure <shoaib_hannure@yahoo.com>
2020-09-01 15:38:13 +0530

    Alert Pop Up Handle Code

--- src/main/java/com/shoaib/selenium/AlertPopUpHandle.java
+++ src/main/java/com/shoaib/selenium/AlertPopUpHandle.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class AlertPopUpHndle {
+public class AlertPopUpHandle {
 
 	public static void main(String[] args) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
